846ab583303df353327c93ddbfd3e6f076e3a220
Stephen Settle <ssettle3@gmail.com>
2015-01-18 21:31:39 -0500

    Digitl Clock Set Up

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Digital Clock using JavaScript
+Part of HW 7 for TIY
---
+++ main.js
@@ -0,0 +1,44 @@
+// This proved to be very difficult for me....
+
+var digClock = document.getElementById("clock")
+
+var time = function() {
+
+	var today = new Date();
+
+	var h = today.getHours().toString();
+	var m = today.getMinutes().toString();
+	var s = today.getSeconds().toString();
+	var amPM = "";
+
+	if(m.length < 2){
+		m = "0" + m;
+	} else{
+		m = m;
+	}
+
+	if(s.length < 2){
+		s = "0" + s;
+	} else {
+		s = s;
+	}
+
+	if(h < 12){
+		amPM = "AM";
+	} else{
+		amPM = "PM";
+	}
+
+	if(h > 12){
+		h = h - 12;
+	} else {
+		h = h;
+	}
+
+
+	clock.innerText = h + ":" + m + ":" + s + " " + amPM;
+
+
+}
+
+window.setInterval(time, 1000);
